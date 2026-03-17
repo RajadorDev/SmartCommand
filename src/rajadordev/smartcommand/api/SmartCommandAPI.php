b6b4554dcc8f74ab1ea2f82e1760fb3d504bf9b4
RajadorDev <rajadordeveloper@gmail.com>
2026-03-17 18:40:54 -0300

    Commands auto complete support

--- src/rajadordev/smartcommand/api/SmartCommandAPI.php
+++ src/rajadordev/smartcommand/api/SmartCommandAPI.php
@@ -19,12 +19,27 @@ declare (strict_types=1);
 
 namespace rajadordev\smartcommand\api;
 
+use pocketmine\command\Command;
 use Throwable;
 use RuntimeException;
 use pocketmine\Server;
 use pocketmine\plugin\Plugin;
 use pocketmine\command\CommandSender;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\network\mcpe\protocol\serializer\AvailableCommandsPacketAssembler;
+use pocketmine\network\mcpe\protocol\serializer\AvailableCommandsPacketDisassembler;
+use pocketmine\network\mcpe\protocol\types\command\CommandHardEnum;
+use pocketmine\network\mcpe\protocol\types\command\CommandOverload;
+use pocketmine\network\mcpe\protocol\types\command\CommandParameter;
+use pocketmine\player\Player;
+use rajadordev\smartcommand\command\argument\Argument;
+use rajadordev\smartcommand\command\argument\StringListArgument;
 use rajadordev\smartcommand\message\DefaultMessages;
+use rajadordev\smartcommand\command\SmartCommand;
+use rajadordev\smartcommand\command\subcommand\BaseSubCommand;
+use rajadordev\smartcommand\listener\SmartCommandPacketHandler;
+use ReflectionClass;
+use rajadordev\smartcommand\command\argument\BoolArgument;
 
 final class SmartCommandAPI
 {
@@ -34,6 +49,14 @@ final class SmartCommandAPI
     const POCKETMINE_API = ['^5.0.0'];
 
     const GLOBAL_PREFIX_ID = 'SmartCommand-';
+
+    const PARAM_NETWORK_TYPE_IDS = [
+        'text' => AvailableCommandsPacket::ARG_TYPE_RAWTEXT,
+        'integer' => AvailableCommandsPacket::ARG_TYPE_INT,
+        'float' => AvailableCommandsPacket::ARG_TYPE_FLOAT,
+        /** It was write error fixed since 2.3.0, but plugins using SmartCommand lower than 2.3.0 will keep using this keyword */
+        'interger' => AvailableCommandsPacket::ARG_TYPE_INT
+    ];
 
     /** @var string */
     private static string $commandErrorFolder, $commandErrorFile;
@@ -77,10 +100,11 @@ final class SmartCommandAPI
      */
     public static function register(Plugin $plugin) : void
     {
-        if (self::$registeredBy)
-        {
+        if (self::$registeredBy) {
             throw new RuntimeException('SmartCommand is already registered');
         }
+
+
         self::$registeredBy = $plugin;
         $folder = self::getFrameworkFolder();
         self::$commandErrorFolder = $folder . DIRECTORY_SEPARATOR . 'error' . DIRECTORY_SEPARATOR;
@@ -157,6 +181,8 @@ final class SmartCommandAPI
             ];
             DefaultMessages::init($folder . 'messages' . DIRECTORY_SEPARATOR, $defaultMessagesList);
         }
+
+        SmartCommandPacketHandler::register($plugin);
     }
 
     /**
@@ -181,6 +207,147 @@ final class SmartCommandAPI
     public static function getFrameworkFolder() : string 
     {
         return Server::getInstance()->getDataPath() . 'smartcommand' . DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * @internal
+     *
+     * @param AvailableCommandsPacket $packet
+     * @param Player $sender
+     * @return AvailableCommandsPacket|null
+     */
+    public static function putCommandTips(AvailableCommandsPacket $packet, Player $sender) : ?AvailableCommandsPacket 
+    {
+        $changed = false;
+        $disassemblePacket = AvailableCommandsPacketDisassembler::disassemble($packet);
+        foreach ($disassemblePacket->commandData as $singleCommandData) {
+            $commandName = $singleCommandData->getName();
+            $command = Server::getInstance()->getCommandMap()->getCommand($commandName);
+
+            if ($command instanceof Command && static::isSmartCommandInstance($command) && $command->testPermissionSilent($sender)) {
+                $overloads = self::generateCommandOverloads($command, $sender);
+                $singleCommandData->overloads = $overloads;
+                $changed = true;
+            }
+        }
+
+        if ($changed) {
+            return AvailableCommandsPacketAssembler::assemble($disassemblePacket->commandData, [], []);
+        }
+        return null;
+    }
+
+    /**
+     * @internal Check if the command given is a real SmartCommand instance even if in another namespace
+     *
+     * @param Command $command
+     * @param-out SmartCommand $command
+     * @return boolean
+     */
+    private static function isSmartCommandInstance(Command &$command) : bool 
+    {
+        return (method_exists($command, 'executeSubCommand') && method_exists($command, 'testPermission') && method_exists($command, 'onRun'));
+    }
+
+    /**
+     * @internal
+     * @param Command $command
+     * @param Player $player
+     * @return CommandOverload[]
+     */
+    private static function generateCommandOverloads(Command $command, Player $player) : array
+    {
+        $overloads = [];
+        /** @var SmartCommand $command */
+        foreach ($command->getSubCommands() as $subCommand) {
+            if (!$player->hasPermission($subCommand->getPermission())) {
+                continue;
+            }
+
+            $subCommandName = $subCommand->getName();
+
+            $subCommandParam = CommandParameter::enum(
+                $subCommandName,
+                new CommandHardEnum($subCommandName, [$subCommandName]),
+                flags: 0,
+                optional: false
+            );
+
+            if (method_exists($subCommand, 'getArgument') && !is_null($argumentsOverloads = self::generateArgumentableOverloads($subCommand))) {
+                foreach ($argumentsOverloads as $overload) {
+                    $overloads[] = new CommandOverload(false, [$subCommandParam, ...$overload->getParameters()]);
+                }
+            } else {
+                $overloads[] = new CommandOverload(false, [$subCommandParam]);
+            }
+
+        }
+
+        if (empty($overloads)) {
+            if ($commandOverloads = self::generateArgumentableOverloads($command)) {
+                foreach ($commandOverloads as $commandOverload) {
+                    $overloads[] = $commandOverload;
+                }
+            }
+        }
+
+
+        return $overloads;
+    }
+
+    /**
+     * @internal 
+     * @param SmartCommand|BaseSubCommand $command
+     * @return CommandOverload[]|null
+     */
+    private static function generateArgumentableOverloads(mixed $command) : ?array
+    {
+        $params = [];
+        for ($index = 0; true; $index++) {
+            if ($argument = $command->getArgument($index)) {
+                $params[] = self::generateArgumentNetworkParam($argument);
+                continue;
+            }
+            break;
+        }
+
+        if (empty($params)) {
+            return null;
+        }
+        return [new CommandOverload(false, $params)];
+    }
+
+    /**
+     * @param Argument $argument
+     * @return CommandParameter
+     */
+    private static function generateArgumentNetworkParam(mixed $argument) : CommandParameter
+    {
+        $argumentTypeName = $argument->getTypeName();
+        $optionalArgument = !$argument->isRequired();
+
+        if (!isset(self::PARAM_NETWORK_TYPE_IDS[$argumentTypeName])) {
+
+            $hasStringList = str_contains($argument->getTypeName(), '|');
+            if ($hasStringList) {
+                $typeListName = $argument->getTypeName();
+                $enumTypeList = explode('|', $typeListName);
+                return CommandParameter::enum(
+                    $argument->getName(), 
+                    new CommandHardEnum(
+                        $argument->getTypeName(),
+                        $enumTypeList
+                    ),
+                    flags: 0,
+                    optional: $optionalArgument
+                );
+            }
+            $argumentTypeNetwork = AvailableCommandsPacket::ARG_TYPE_STRING;
+        } else {
+            $argumentTypeNetwork = self::PARAM_NETWORK_TYPE_IDS[$argumentTypeName];
+        }
+
+        return CommandParameter::standard($argument->getName(), $argumentTypeNetwork, 0, $optionalArgument);
     }
     
 }
---
+++ src/rajadordev/smartcommand/listener/SmartCommandPacketHandler.php
@@ -0,0 +1,98 @@
+<?php
+
+declare (strict_types=1);
+ 
+/***
+ *   
+ * Rajador Developer
+ * 
+ * ▒█▀▀█ ░█▀▀█ ░░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█▀▀█ 
+ * ▒█▄▄▀ ▒█▄▄█ ░▄░▒█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▀ 
+ * ▒█░▒█ ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█░▒█
+ * 
+ * GitHub: https://github.com/rajadordev
+ * 
+ * Discord: rajadortv
+ * 
+ * 
+**/ 
+
+namespace rajadordev\smartcommand\listener;
+
+use pocketmine\event\HandlerListManager;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\plugin\Plugin;
+use pocketmine\Server;
+use rajadordev\smartcommand\api\SmartCommandAPI;
+use RuntimeException;
+
+final class SmartCommandPacketHandler implements Listener
+{
+
+    public const VERSION = '1.0.0';
+
+    public const HANDLER_VERSION_IDENTIFIER = '__SmartCommand-Handler';
+    
+    /** @var Plugin */
+    private static Plugin $registeredBy;
+
+    /** @var boolean */
+    private static bool $sendingAvaliableCommands = false;
+
+    /**
+     * @param Plugin $plugin
+     * @return void
+     * @throws RuntimeException
+     */
+    public static function register(Plugin $plugin) : void
+    {
+        if (isset(self::$registeredBy)) {
+            throw new RuntimeException("Can't register SmartCommandPacketHandler cause it's already registered");
+        }
+        self::$registeredBy = $plugin;
+        $logger = Server::getInstance()->getLogger();
+        if (isset($GLOBALS[self::HANDLER_VERSION_IDENTIFIER])) {
+            if (!version_compare(self::VERSION, $GLOBALS[self::HANDLER_VERSION_IDENTIFIER]::VERSION, '>')) {
+                $logger->debug("A SmartCommandPacket handler is already registered");
+                return;
+            }
+            /** Replacing the outdated PacketHandler */
+            $oldPacketHandler = $GLOBALS[self::HANDLER_VERSION_IDENTIFIER];
+            $oldVersion = $oldPacketHandler::VERSION;
+            $logger->debug("Unregistering old handler version ($oldVersion)...");
+            HandlerListManager::global()->unregisterAll($oldPacketHandler);
+        }
+        Server::getInstance()->getPluginManager()->registerEvents(new self, $plugin);
+        $version = self::VERSION;
+        $logger->debug("SmartCommandPacket handler $version registered");
+    }
+
+    /**
+     * @priority HIGH
+     * @ignoreCancelled TRUE
+     */
+    public function sendCommandPacket(DataPacketSendEvent $event) : void
+    {
+        if (self::$sendingAvaliableCommands) {
+            return;
+        }
+
+        $packets = $event->getPackets();
+        foreach ($packets as $index => $packet) {
+            if ($packet instanceof AvailableCommandsPacket) {
+                unset($packets[$index]);
+                foreach ($event->getTargets() as $target) {
+                    if (is_null($newPacket = SmartCommandAPI::putCommandTips($packet, $target->getPlayer()))) {
+                        $newPacket = $packet;
+                    }
+                    self::$sendingAvaliableCommands = true;
+                    $target->sendDataPacket($newPacket, false);
+                    self::$sendingAvaliableCommands = false;
+                }
+            }
+        }
+        $event->setPackets($packets);
+    }
+}
