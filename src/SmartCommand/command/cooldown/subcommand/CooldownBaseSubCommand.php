1e6eb3e1200eeda7353672ad707f51ac1b10828e
RajadorDev <rajadordeveloper@gmail.com>
2026-05-30 21:00:10 -0300

    Removed "Diamond API" doc text

--- src/SmartCommand/command/cooldown/subcommand/CooldownBaseSubCommand.php
+++ src/SmartCommand/command/cooldown/subcommand/CooldownBaseSubCommand.php
@@ -4,7 +4,7 @@ declare (strict_types=1);
  
 /***
  *   
- * Rajador Developer Diamond API
+ * Rajador Developer 
  * 
  *  ██████╗  █████╗      ██╗ █████╗ ██████╗  ██████╗ ██████╗ 
  *  ██╔══██╗██╔══██╗     ██║██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
